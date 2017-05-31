b559c8cedbe47840c4b18e4fa6ba8a67301d1617
Victor Vlasenko <victor.vlasenko@sysgears.com>
2017-05-31 08:04:14 +0300

    Ignore child compilations.
    This prevents issues with ExtractTextPlugin

--- index.js
+++ index.js
@@ -41,8 +41,8 @@ PersistGraphQLPlugin.prototype.apply = function(compiler) {
   self.virtualModules.apply(compiler);
   self._compiler = compiler;
 
-  compiler.plugin('compilation', function() {
-    if (!self._queryMap) {
+  compiler.plugin('compilation', function(compilation) {
+    if (!self._queryMap && !compilation.compiler.parentCompilation) {
       self.virtualModules.writeModule(self.options.moduleName, '{}');
     }
   });
@@ -66,75 +66,79 @@ PersistGraphQLPlugin.prototype.apply = function(compiler) {
 
   if (!self.options.provider) {
     compiler.plugin('compilation', function(compilation) {
-      compilation.plugin('seal', function() {
-        var graphQLString = '';
-        var allQueries = [];
-        compilation.modules.forEach(function(module) {
-          var queries = module._graphQLQueries;
-          if (queries) {
+      if (!compilation.compiler.parentCompilation) {
+        compilation.plugin('seal', function() {
+          var graphQLString = '';
+          var allQueries = [];
+          compilation.modules.forEach(function(module) {
+            var queries = module._graphQLQueries;
+            if (queries) {
+              Object.keys(queries).forEach(function(query) {
+                allQueries.push(query);
+              });
+            } else if (module._graphQLString) {
+              graphQLString += module._graphQLString;
+            }
+          });
+
+          if (graphQLString) {
+            var extractor = new ExtractGQL({inputFilePath: '',
+              queryTransformers: self.options.addTypename ? [function(doc) {
+              return addTypenameTransformer(JSON.parse(JSON.stringify(doc)));
+            }] : undefined});
+
+            var doc = graphql.parse(graphQLString);
+            var docMap = graphql.separateOperations(doc);
+            var queries = {};
+            Object.keys(docMap).forEach(function (operationName) {
+              var document = docMap[operationName];
+              var fragmentMap = {};
+              for (var i = document.definitions.length - 1; i >= 0; i--) {
+                var def = document.definitions[i];
+                if (def.kind === 'FragmentDefinition') {
+                  if (!fragmentMap[def.name.value]) {
+                    fragmentMap[def.name.value] = true;
+                  } else {
+                    document.definitions.splice(i, 1);
+                  }
+                }
+              }
+              queries = _.merge(queries, extractor.createMapFromDocument(document));
+            });
+
             Object.keys(queries).forEach(function(query) {
               allQueries.push(query);
             });
-          } else if (module._graphQLString) {
-            graphQLString += module._graphQLString;
           }
-        });
 
-        if (graphQLString) {
-          var extractor = new ExtractGQL({inputFilePath: '',
-            queryTransformers: self.options.addTypename ? [function(doc) {
-            return addTypenameTransformer(JSON.parse(JSON.stringify(doc)));
-          }] : undefined});
+          var mapObj = {};
+          var id = 1;
 
-          var doc = graphql.parse(graphQLString);
-          var docMap = graphql.separateOperations(doc);
-          var queries = {};
-          Object.keys(docMap).forEach(function (operationName) {
-            var document = docMap[operationName];
-            var fragmentMap = {};
-            for (var i = document.definitions.length - 1; i >= 0; i--) {
-              var def = document.definitions[i];
-              if (def.kind === 'FragmentDefinition') {
-                if (!fragmentMap[def.name.value]) {
-                  fragmentMap[def.name.value] = true;
-                } else {
-                  document.definitions.splice(i, 1);
-                }
+          allQueries.sort().forEach(function(query) {
+            mapObj[query] = id++;
+          });
+
+          var newQueryMap = JSON.stringify(mapObj);
+          if (newQueryMap !== self._queryMap) {
+            self._queryMap = newQueryMap;
+            self.virtualModules.writeModule(self.options.moduleName, self._queryMap);
+            compilation.modules.forEach(function(module) {
+              if (module.resource === self.options.moduleName ||
+                module.resource === path.resolve(path.join(compiler.context, self.options.moduleName))) {
+                module._source._value = "module.exports = " + self._queryMap + ";";
               }
-            }
-            queries = _.merge(queries, extractor.createMapFromDocument(document));
-          });
-
-          Object.keys(queries).forEach(function(query) {
-            allQueries.push(query);
-          });
-        }
-
-        var mapObj = {};
-        var id = 1;
-
-        allQueries.sort().forEach(function(query) {
-          mapObj[query] = id++;
+            });
+          }
+          self._listeners.forEach(function(listener) { listener._notify(self._queryMap); });
         });
-
-        var newQueryMap = JSON.stringify(mapObj);
-        if (newQueryMap !== self._queryMap) {
-          self._queryMap = newQueryMap;
-          self.virtualModules.writeModule(self.options.moduleName, self._queryMap);
-          compilation.modules.forEach(function(module) {
-            if (module.resource === self.options.moduleName ||
-              module.resource === path.resolve(path.join(compiler.context, self.options.moduleName))) {
-              module._source._value = "module.exports = " + self._queryMap + ";";
-            }
-          });
-        }
-        self._listeners.forEach(function(listener) { listener._notify(self._queryMap); });
-      });
+      }
     });
   }
   if (self.options.filename) {
     compiler.plugin('after-compile', function(compilation, callback) {
-      compilation.assets[self.options.filename] = new RawSource(self._queryMap);
+      if (!compilation.compiler.parentCompilation) {
+        compilation.assets[self.options.filename] = new RawSource(self._queryMap);
+      }
       callback();
     });
   }
